a055861d28dcd87c7c37ec2df5d7e2f9c197e8e3
etozhectnz <96590871+etozhectnz@users.noreply.github.com>
2022-01-13 03:14:02 +0200

    add hw 8

---
+++ hw8/index.js
@@ -0,0 +1,33 @@
+let str = 'Вася;Петя;Вова;Олег'
+let arr = str.split(";")
+console.log(arr)
+
+hello2('Vasya')
+
+
+
+function hello2 (name){
+    console.log('Hello', name)
+}
+
+const val = addOneForAll(1,2,3,4)
+console.log(val)
+
+function addOneForAll (...args){
+    for(let i = 0; i < args.length; i++){
+        args[i] ++
+    }
+    return args
+}
+
+
+
+var i = 0;
+while(true) {
+    if(Math.random() > 0.9) {
+        alert(++i)
+        break
+    } else {
+        i++
+    } 
+}
